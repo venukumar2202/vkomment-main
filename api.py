c88a351bc534bce908aad0dae619579791e504f3
venukumar2202 <149363915+venukumar2202@users.noreply.github.com>
2023-11-09 11:14:16 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from fetch_html import fetch_html
+from fetch_html import fetch_html  # Import your defined functions
 from extract_product_info import get_product_info
 from scrape_reviews import scrape_reviews, save_to_csv
 from generate_summary import generate_summary_from_csv
@@ -12,51 +12,45 @@ CORS(app, resources={r"/execute_main_script": {"origins": "*"}})  # All origins 
 def execute_main_script():
     url = request.args.get('url')  # Get the URL from the query parameter
 
-    # Fetch product information
-    soup = fetch_html(url)
+    # Logic to fetch product information
+    soup = fetch_html(url)  # Function to fetch HTML from the provided URL
     
     if soup:
+        # Logic to extract information from 'soup'
         title, image_links, price, rating, num_reviews, description = get_product_info(soup)
 
         if num_reviews != "Number of Reviews not found":
-            review_url = None
-            elements = soup.find_all(class_="_3UAT2v _16PBlm")[:3]
+            # Logic to fetch and process reviews
+            review_url = "https://flipkart.com/sample-review"  # Replace this with the URL of reviews
+            reviews = [{"review_id": 1, "review_text": "Great product!"}]  # Replace this with actual reviews
+            csv_file_name = "product_reviews.csv"  # Your CSV file name
 
-            for element in elements:
-                parent_a = element.find_parent('a')
-                if parent_a and 'href' in parent_a.attrs:
-                    href = parent_a['href']
-                    review_url = 'https://flipkart.com' + href
-                    break
-
-            if review_url:
-                reviews = scrape_reviews(review_url)
-                if reviews:
-                    csv_file_name = "product_reviews.csv"
-                    save_to_csv(reviews, csv_file_name)
-                    
-                    summary = generate_summary_from_csv(csv_file_name, 'Review Description')
-                    summary_text = summary[0]['summary_text'] if (summary and len(summary) > 0 and 'summary_text' in summary[0]) else "Summary not generated"
-                    
-                    product_data = {
-                        "Title": title,
-                        "Image Links": image_links,
-                        "Product Price": price,
-                        "Product Rating": rating,
-                        "Number of Reviews": num_reviews,
-                        "Product Description": description,
-                        "Review Summary": summary_text
-                    }
-                    
-                    return jsonify(product_data)
-                else:
-                    return "No reviews found or an error occurred."
-            else:
-                return "Review URL not found."
+            # Generate summary from CSV
+            summary = generate_summary_from_csv(csv_file_name, 'Review Description')
+            summary_text = summary[0]['summary_text'] if (summary and len(summary) > 0 and 'summary_text' in summary[0]) else "Summary not generated"
+            
+            product_data = {
+                "Title": title,
+                "Image Links": image_links,
+                "Product Price": price,
+                "Product Rating": rating,
+                "Number of Reviews": num_reviews,
+                "Product Description": description,
+                "Review Summary": summary_text
+            }
+            
+            return jsonify(product_data)
         else:
             return "No reviews available for this product."
     else:
         return "Failed to retrieve product information."
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'https://your-frontend-domain.com')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)  # Change the port as per your requirements
